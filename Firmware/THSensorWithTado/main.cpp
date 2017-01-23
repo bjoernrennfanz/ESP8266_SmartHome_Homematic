071cffed3dd914e0dadec635f5e4387f4fb518e4
Björn Rennfanz <bjoern.rennfanz@br-edv.net>
2017-01-23 05:40:55 +0100

    First try with ContikiCore processes using ESP8266

--- Firmware/THSensorWithTado/main.cpp
+++ Firmware/THSensorWithTado/main.cpp
@@ -1,12 +1,19 @@
-#include <AS.h>
+#include <stdio.h>
+#include <stddef.h>
 
-AS as;
+#include "contiki-core.h"
 
-void main()
+PROCESS(main_process, "Main process");
+AUTOSTART_PROCESSES(&main_process);
+
+PROCESS_THREAD(main_process, ev, data)
 {
-	as.sendWeatherEvent();
+  PROCESS_BEGIN();
 
+  while(1) {
+    PROCESS_WAIT_EVENT();
+    printf("Got event number %d\n", ev);
+  }
+
+  PROCESS_END();
 }
-
-
-
