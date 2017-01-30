466487ac72e755657b7ec69e90aea885c1025c73
Björn Rennfanz <bjoern@fam-rennfanz.de>
2017-01-30 11:44:29 +0100

    First compiling version with ContikiCore and autostart processes

--- Firmware/THSensorWithTado/main.cpp
+++ Firmware/THSensorWithTado/main.cpp
@@ -1,18 +1,38 @@
 #include <stdio.h>
 #include <stddef.h>
 
+#include <ets_sys.h>
+#include <os_type.h>
+#include <osapi.h>
+
 #include "contiki-core.h"
 
-PROCESS(main_process, "Main process");
-AUTOSTART_PROCESSES(&main_process);
+PROCESS(first_process, "First");
+PROCESS(second_process, "Second");
+AUTOSTART_PROCESSES(&first_process, &second_process);
 
-PROCESS_THREAD(main_process, ev, data)
+PROCESS_THREAD(first_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  while(1) {
-    PROCESS_WAIT_EVENT();
-    printf("Got event number %d\n", ev);
+  while(1)
+  {
+	  PROCESS_WAIT_EVENT();
+	  // os_printf("First: Got event number %d\n", ev);
+  }
+
+  PROCESS_END();
+}
+
+
+PROCESS_THREAD(second_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  while(1)
+  {
+	  PROCESS_WAIT_EVENT();
+	  // os_printf("Second: Got event number %d\n", ev);
   }
 
   PROCESS_END();
